00a45173fe4acc3b0136c03d65f6ed14b5b4f7c1
Daniel Bass <danielbass123456@gmail.com>
2022-04-19 22:33:30 -0500

    Edward

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import ImageLinks from './images.js'
+import './App.css'
+import { Spinner } from 'react-bootstrap'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [edward_image, change_image] = useState("edward_king.jpg")
+  const [loading, change_loading] = useState(false)
+
+  function change_edward_image() {
+    change_loading(true)
+    change_image(ImageLinks[Math.floor(Math.random() * ImageLinks.length)])
+    setTimeout(() => { change_loading(false) }, 200)
+  }
+
+  if (loading) {
+    return (
+      <div>
+        <Spinner animation="border" role="status" variant="primary">
+        </Spinner>
+      </div>
+    )
+  } else {
+    return (
+      <div id="parent" className="App">
+        <Card border="primary" style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={`https://leinad.dev/images/edward/${edward_image}`} />
+          <Card.Body>
+            <Card.Title>Edward the Cat</Card.Title>
+            <Card.Text>
+              <strong>Edward</strong> is a cat and he likes to <small>sit</small> places, not do <u>much</u>, and <code>eat</code>
+            </Card.Text>
+            <Button variant="primary" onClick={change_edward_image}>Edward</Button>
+          </Card.Body>
+        </Card>
+      </div>
+    )
+  };
 }
 
 export default App;
---
+++ src/images.js
@@ -0,0 +1,20 @@
+const image_links = [
+  "edward_chair.jpg",
+  "edward_cheeze.jpg",
+  "edward_christmas.jpg",
+  "edward_curl.jpg",
+  "edward_grump.jpg",
+  "edward_king.jpg",
+  "edward_lay.jpg",
+  "edward_leg.jpg",
+  "edward_majestic.jpg",
+  "edward_nap.jpg",
+  "edward_railing.jpg",
+  "edward_red.jpg",
+  "edward_sit.jpg",
+  "edward_sleep.jpg",
+  "edward_sprawl.jpg",
+  "regal_edward.jpg"
+]
+
+export default image_links
